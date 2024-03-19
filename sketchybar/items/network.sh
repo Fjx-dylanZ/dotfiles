c966b6bf3950608c29a565419e207f5744dc923e
Dylan Zhang <dylanpict@gmail.com>
2024-03-19 13:12:53 -0400

    Fix typo in network.sh

--- sketchybar/items/network.sh
+++ sketchybar/items/network.sh
@@ -3,11 +3,11 @@ sketchybar -m --add       item               network_up right                   
               --set       network_up         label.font="SF Pro:Heavy:9"                                   \
                                              icon.drawing=off                                               \
                                              y_offset=5                                                    \
-                                             width=0                                                       \
+                                             width=0                                                      \
                                              update_freq=2                                                 \
                                              script="~/.config/sketchybar/plugins/network.sh"              \
                                                                                                            \
               --add       item               network_down right                                             \
               --set       network_down       label.font="SF Pro:Heavy:9"                                   \
-                                             icon.drwain=off                                                \
+                                             icon.drawing=off                                                \
                                              y_offset=-5
